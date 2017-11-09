aa9c1dad56362e43e9885de430f64f3c660f95f3
ZHDKk <1083861095@qq.com>
2017-11-09 11:25:57 +0800

    第一次提交

--- UITextFailed/ViewController.h
+++ UITextFailed/ViewController.h
@@ -10,6 +10,13 @@
 
 @interface ViewController : UIViewController
 
+{
+    //定义一个textField
+    //文本输入区域：用户名、密码等需要输入文本文字的内容区域。只能输入单行的文字，不能输入或显示多行
+    UITextField *_textField;
+    
+}
 
+@property (retain,nonatomic) UITextField *_textField;
 @end
 
--- UITextFailed/ViewController.m
+++ UITextFailed/ViewController.m
@@ -13,10 +13,31 @@
 @end
 
 @implementation ViewController
-
+@synthesize  _textField = _textField;
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    _textField = [[UITextField alloc] init];
+    _textField.frame=CGRectMake(100, 100, 200, 40);
+    //设置textField的内容文字
+    _textField.text=@"用户名";
+    
+    _textField.font = [UIFont systemFontOfSize:15];
+    _textField.textColor = [UIColor blackColor];
+    
+    //设置边框风格
+    _textField.borderStyle = UITextBorderStyleRoundedRect;
+    
+    //设置虚拟键盘风格
+    _textField.keyboardType = UIKeyboardTypeDefault;
+    
+    //提示文字信息:相当于hint
+    _textField.placeholder = @"请输入用户名";
+    
+    //是否作为密码输入   YES:作为密码处理。 NO：显示输入的文字
+    _textField.secureTextEntry=YES;
+    [self.view addSubview:_textField];
 }
 
 
